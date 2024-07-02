9c548e9767976aa18fe899b48862a07502e800d0
Yuri Luttsev <floomyz@vk.com>
2024-07-02 16:02:09 +0300

    init

---
+++ src/main/java/ru/luttsev/contractors/ContractorsRestApiApplication.java
@@ -0,0 +1,13 @@
+package ru.luttsev.contractors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContractorsRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ContractorsRestApiApplication.class, args);
+    }
+
+}
