8f2da26692a2517017c244c5ea488b9b21f2f1b9
Yuri Luttsev <floomyz@vk.com>
2024-08-23 15:40:50 +0300

    refactor: обновлен SQL файл для тестов

--- src/test/resources/sql/contractors_test.sql
+++ src/test/resources/sql/contractors_test.sql
@@ -1,4 +1,4 @@
-insert into contractor (id, parent_id, name, name_full, inn, ogrn, country, industry, org_form, is_active)
+insert into contractor (id, parent_id, name, name_full, inn, ogrn, country, industry, org_form, is_active, active_main_borrower)
 values
-    ('1', null, 'Contractor 1', 'Contractor 1', '12345678', '12345678', 'RUS', 1, 2, true),
-    ('2', null, 'Contractor 2', 'Contractor 2', '123456', '123456', 'RUS', 1, 2, true);
+    ('1', null, 'Contractor 1', 'Contractor 1', '12345678', '12345678', 'RUS', 1, 2, true, false),
+    ('2', null, 'Contractor 2', 'Contractor 2', '123456', '123456', 'RUS', 1, 2, true, false);
